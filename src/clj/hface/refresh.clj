a40b37e7eae152dbe9fd6099cfe1c7652c861a56
anatoly <tolitius@gmail.com>
2015-02-28 17:18:31 -0500

    adding rates to q-stats

--- src/clj/hface/refresh.clj
+++ src/clj/hface/refresh.clj
@@ -18,31 +18,39 @@
             :put-rate (rate-it pm cm :put-count :put-rate interval)
             :hit-rate (rate-it pm cm :hits :hit-rate interval)))
 
-(defn compare-and-rate-maps [previous current interval]
+(defn rate-q [pm cm interval]
+  (assoc cm :take-rate (rate-it pm cm :number-of-offers :take-rate interval)
+            :put-rate (rate-it pm cm :number-of-polls :put-rate interval)
+            :rejected-put-rate (rate-it pm cm :number-of-rejected-offers :rejected-put-rate interval)
+            :empty-take-rate (rate-it pm cm :number-of-empty-polls :empty-take-rate interval)))
+
+(defn compare-and-rate [rate previous current interval]
   (into {}
     (for [[k v] current]
-      (if-let [pv (k previous)]           ;; if there is a value in a prev map for this key
-        [k (rate-map pv v interval)]
-        [k (rate-map v v interval)]))))
+      (if-let [pv (k previous)]          ;; if there is a value in a prev map for this key
+        [k (rate pv v interval)]
+        [k (rate v v interval)]))))
 
-(defn add-map-ratings [p-stats            ;; previous stats
-                       c-stats            ;; current stats
-                       interval
-                       mtype]             ;; :map-stats, :multi-map-stats
-  (let [m-stats #(-> % :aggregated mtype)
-        previous (m-stats p-stats)
-        current (m-stats c-stats)]
-    (assoc-in c-stats [:aggregated mtype]
-              (compare-and-rate-maps previous current interval))))
+(defn add-ratings [c-stats               ;; current stats
+                   p-stats               ;; previous stats
+                   rate
+                   interval
+                   s-type]               ;; :map-stats, :multi-map-stats, :queue-stats
+  (let [stats #(-> % :aggregated s-type)
+        previous (stats p-stats)
+        current (stats c-stats)]
+    (assoc-in c-stats [:aggregated s-type]
+              (compare-and-rate rate previous current interval))))
 
 ;;-----------------------------------------------------------------
 
 (defn refresh-stats 
   "refreshing stats and adding rate metrics"
   [previous interval]
-  (let [current (cluster-stats)
-        with-map-rates (add-map-ratings @previous current interval :map-stats)
-        with-rates (add-map-ratings @previous with-map-rates interval :multi-map-stats)]     ;; TODO: refactor out and add other ratings
+  (let [with-rates (-> (cluster-stats)
+                       (add-ratings @previous rate-map interval :map-stats)
+                       (add-ratings @previous rate-map interval :multi-map-stats)
+                       (add-ratings @previous rate-q interval :queue-stats))]
     (reset! previous with-rates)))
 
 (defn collect-stats 
