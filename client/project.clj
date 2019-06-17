136ba5f6437dbb0733ca851f90083d00a17b6529
anatoly <tolitius@gmail.com>
2019-06-17 11:58:51 -0400

    hface-client to 3.12.1

--- client/project.clj
+++ client/project.clj
@@ -1,4 +1,4 @@
-(defproject org.hface/hface-client "0.1.5"
+(defproject org.hface/hface-client "0.1.6"
   :description "look your Hazelcast cluster in the face!"
   :url "https://github.com/tolitius/hface"
   :license {:name "Eclipse Public License"
@@ -6,14 +6,14 @@
 
   :java-source-paths ["src/java"]
 
-  :dependencies [[com.hazelcast/hazelcast "3.9"]]
+  :dependencies [[com.hazelcast/hazelcast "3.12.1"]]
 
   ;; ceremony for maven central
 
   :scm {:name "git"
         :url "https://github.com/tolitius/hface.git"}
 
-  :pom-addition [:developers [:developer 
+  :pom-addition [:developers [:developer
                                [:id "tolitius"]
                                [:name "Anatoly"]
                                [:url "https://github.com/tolitius"]]]
@@ -22,7 +22,7 @@
                                     :creds :gpg}
                         "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
                                      :creds :gpg}}
-            
+
   :classifiers {:javadoc {:java-source-paths ^:replace ["src/java"]
                           :source-paths ^:replace []
                           :resource-paths ^:replace []}
